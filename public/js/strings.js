e52e21d6ed3ed07209807117dcc5e227a3a263bb
aria <ben@benfromthe.online>
2026-01-16 06:27:34 -0500

    Okay last one for real.......

--- public/js/strings.js
+++ public/js/strings.js
@@ -7,7 +7,7 @@ window.AppStrings = {
     passphrasePlaceholder: 'the-fun-zone',
     rememberRoom: 'Auto-join?',
     openBoard: 'Join Room',
-    hintHtml: 'a little thang made by aria unicornfan <img src="assets/beandog.png" alt="beandog" width="32" height="18" style="vertical-align: text-bottom; margin-left: 4px; display: inline-block;" /><br><br>client-side encrypted collaborative sticky note board. post some notes for yourself or post with pals! nobody can snoop (not even me!) unless they know the exact room name! keep it secret like a password...<br><br>...or just roll your own server if you wanna! -> <a href="https://github.com/0x61726961/mynotes" target="_blank" rel="noopener noreferrer">source</a>'
+    hintHtml: 'a little thang made by aria unicornfan <img src="assets/beandog.png" alt="beandog" width="32" height="18" style="vertical-align: text-bottom; margin-left: 4px; display: inline-block;" /><br><br>client-side encrypted collaborative sticky note board. post some notes for yourself or post with pals! nobody can snoop (not even me!) unless they know the exact room name! keep it secret like a password...<br><br>...or just roll your own and do whatever you want! -> <a href="https://github.com/0x61726961/mynotes" target="_blank" rel="noopener noreferrer">source</a>'
   },
   board: {
     leaveBoardTitle: 'Leave Room',
